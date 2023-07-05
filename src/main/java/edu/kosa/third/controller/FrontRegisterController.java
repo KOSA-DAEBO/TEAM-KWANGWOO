780b9ffc71ac763104df95dfdf20eca15a3d67e5
yellowrel <ghkdtmdwns12@gmail.com>
2023-07-05 19:06:21 +0900

    고객 회원가입 기능 구현

--- src/main/java/edu/kosa/third/controller/FrontRegisterController.java
+++ src/main/java/edu/kosa/third/controller/FrontRegisterController.java
@@ -14,6 +14,7 @@ import edu.kosa.third.action.Action;
 import edu.kosa.third.action.ActionForward;
 import edu.kosa.third.dao.LeaveDao;
 import edu.kosa.third.dto.LeaveDto;
+import edu.kosa.third.service.JoinOkServiceAction;
 import edu.kosa.third.service.LeaveApplyServiceAction;
 
 @WebServlet("*.do")
