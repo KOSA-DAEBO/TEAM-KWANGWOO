8ba04f40a4569b51a2855af195da883d93c681bb
Jaejin Lee <jaejin1112@gmail.com>
2023-07-05 12:11:22 +0900

    add Getter Setter in CustomerDto

--- src/main/java/edu/kosa/third/dto/CustomerDto.java
+++ src/main/java/edu/kosa/third/dto/CustomerDto.java
@@ -7,4 +7,60 @@ public class CustomerDto {
 	private String customerEmail, customerTel, customerAddr, customerName;
 	private boolean customerGender;
 	private Date customerBirth;
+
+	public int getCustomerNo() {
+		return customerNo;
+	}
+
+	public void setCustomerNo(int customerNo) {
+		this.customerNo = customerNo;
+	}
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerTel() {
+		return customerTel;
+	}
+
+	public void setCustomerTel(String customerTel) {
+		this.customerTel = customerTel;
+	}
+
+	public String getCustomerAddr() {
+		return customerAddr;
+	}
+
+	public void setCustomerAddr(String customerAddr) {
+		this.customerAddr = customerAddr;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public boolean isCustomerGender() {
+		return customerGender;
+	}
+
+	public void setCustomerGender(boolean customerGender) {
+		this.customerGender = customerGender;
+	}
+
+	public Date getCustomerBirth() {
+		return customerBirth;
+	}
+
+	public void setCustomerBirth(Date customerBirth) {
+		this.customerBirth = customerBirth;
+	}
 }
