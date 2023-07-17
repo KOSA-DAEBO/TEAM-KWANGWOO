85ce791f95c6ac9f8432b6e06811aa50f60d1565
Jaejin Lee <jaejin1112@gmail.com>
2023-07-17 14:43:08 +0900

    Img file upload success but need making Thumbnail

--- src/main/java/edu/kosa/third/service/addProductServiceAction.java
+++ src/main/java/edu/kosa/third/service/addProductServiceAction.java
@@ -2,33 +2,56 @@ package edu.kosa.third.service;
 
 import java.io.File;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import edu.kosa.third.action.Action;
 import edu.kosa.third.action.ActionForward;
+import edu.kosa.third.utils.Thumbnail;
 
 public class addProductServiceAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward forward = new ActionForward();
-		forward.setRedirect(false);
+        forward.setRedirect(false);
 
-		try {
-			Part filePart = request.getPart("file");
-			String fileName = filePart.getSubmittedFileName();
-			String savePath = "./images"; // 파일 저장 경로 지정
-			
-			filePart.write(savePath + File.separator + fileName);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		forward.setPath("productList.do");
-		return forward;
+        String realPath = "";
+        String realPath2 = "";
+        String savePath = "./images"; // 원본 이미지 저장 경로
+        String thumbnailPath = "./thumbnail"; // 썸네일 이미지 저장 경로
+        String encType = "UTF-8";
+        int maxSize = 1024 * 1024 * 5; //file upload 할 수 있는 최대 file 용량 - 5MB
+
+        try {
+        	ServletContext context = request.getServletContext();
+        	realPath = context.getRealPath(savePath);
+        	realPath2 = context.getRealPath(thumbnailPath);
+
+        	MultipartRequest multi = new MultipartRequest(request, realPath, maxSize, encType, new DefaultFileRenamePolicy());
+        	
+        	String originalFileName = multi.getOriginalFileName("file");
+        	int index = originalFileName.indexOf(".");
+        	String fileName = originalFileName.substring(0, index);
+        	String fileExt = originalFileName.substring(index + 1);
+        	
+        	File file = new File(originalFileName);
+        	
+        	String original = realPath + "\\" + fileName;
+        	String thumbnail = realPath2 + "\\" + fileName;
+        	
+        	Thumbnail.createImage(original, thumbnail, fileExt);
+        	
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        forward.setPath("productList.do");
+        return forward;
 	}
 
 }
